92f14ec4b6edcebceb97a5f85ff823ed151a2194
braceal <braceal@umich.edu>
2021-06-04 11:56:15 -0400

    training script for AAE3d

---
+++ examples/point_3d_aae/config.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+from typing import Optional, List
+from mdlearn.utils import BaseSettings, WandbConfig, OptimizerConfig, SchedulerConfig
+
+
+class Point3dAAEConfig(BaseSettings):
+    # File paths
+    # Path to HDF5 training file
+    input_path: Path = Path("TODO")
+    # Path to directory where trainer should write to (cannot already exist)
+    output_path: Path = Path("TODO")
+    # Optionally resume training from a checkpoint file
+    resume_checkpoint: Optional[Path] = None
+
+    # Number of points per sample. Should be smaller or equal
+    # than the total number of points.
+    num_points: int = 28
+    # Number of additional per-point features in addition to xyz coords.
+    num_features: int = 0
+    # Name of the dataset in the HDF5 file.
+    dataset_name: str = "point_cloud"
+    # Name of scalar datasets in the HDF5 file.
+    scalar_dset_names: List[str] = []
+    # If True, subtract center of mass from batch and shift and scale
+    # batch by the full dataset statistics.
+    cms_transform: bool = False
+    # Sets requires_grad torch.Tensor parameter for scalars specified
+    # by scalar_dset_names. Set to True, to use scalars for multi-task
+    # learning. If scalars are only required for plotting, then set it as False.
+    scalar_requires_grad: bool = False
+    # Whether to pull all the training data into memory or read each
+    # batch from disk on the fly
+    in_memory: bool = True
+    # Percentage of data to be used as training data after a random split.
+    split_pct: float = 0.8
+    # Random seed for shuffling train/validation data
+    seed: int = 333
+    # Whether or not to shuffle train/validation data
+    shuffle: bool = True
+    # Number of epochs to train
+    epochs: int = 10
+    # Training batch size
+    batch_size: int = 64
+    # Gradient clipping (max_norm parameter of torch.nn.utils.clip_grad_norm_)
+    clip_grad_max_norm: float = 5.0
+    # Pretrained model weights
+    init_weights: Optional[str] = None
+    # AE (encoder/decoder) optimizer params
+    ae_optimizer: OptimizerConfig = OptimizerConfig(name="Adam", hparams={"lr": 0.0001})
+    # Discriminator optimizer params
+    disc_optimizer: OptimizerConfig = OptimizerConfig(
+        name="Adam", hparams={"lr": 0.0001}
+    )
+    # Learning rate scheduler params
+    scheduler: Optional[SchedulerConfig] = None
+    # Wandb params
+    wandb: Optional[WandbConfig] = WandbConfig()
+
+    # Model hyperparameters
+    latent_dim: int = 20
+    encoder_bias: bool = True
+    encoder_relu_slope: float = 0.0
+    encoder_filters: List[int] = [64, 128, 256, 256, 512]
+    encoder_kernels: List[int] = [5, 5, 3, 1, 1]
+    decoder_bias: bool = True
+    decoder_relu_slope: float = 0.0
+    decoder_affine_widths: List[int] = [64, 128, 512, 1024]
+    discriminator_bias: bool = True
+    discriminator_relu_slope: float = 0.0
+    discriminator_affine_widths: List[int] = [512, 128, 64]
+    # Mean of the prior distribution
+    noise_mu: float = 0.0
+    # Standard deviation of the prior distribution
+    noise_std: float = 1.0
+    # Releative weight to put on gradient penalty
+    lambda_gp: float = 10.0
+    # Releative weight to put on reconstruction loss
+    lambda_rec: float = 0.5
+
+    # Training settings
+    # Number of data loaders for training
+    num_data_workers: int = 0
+    # Number of samples loaded in advance by each worker
+    prefetch_factor: int = 2
+    # Whether or not to ignore the GPU while training.
+    ignore_gpu: bool = False
+    # Log checkpoint file every `checkpoint_log_every` epochs
+    checkpoint_log_every: int = 1
+
+
+if __name__ == "__main__":
+    Point3dAAEConfig().dump_yaml("point_3d_aae_template.yaml")
