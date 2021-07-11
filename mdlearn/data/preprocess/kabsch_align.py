c8910567a0f5e77d740ca4977a28a63014cefc7b
braceal <braceal@umich.edu>
2021-07-11 10:19:54 -0400

    clean up kabsch

--- mdlearn/data/preprocess/kabsch_align.py
+++ mdlearn/data/preprocess/kabsch_align.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 
-def kabsch(toXYZ: np.ndarray, fromXYZ: np.ndarray):
+def kabsch(to_xyz: np.ndarray, from_xyz: np.ndarray):
     r"""Aligns a single frame :obj:`fromXYZ` to another frame :obj:`toXYZ`
     using the kabsch method.
 
     Parameters
     ----------
-    toXYZ : np.ndarray
+    to_xyz : np.ndarray
         3 x N array of coordinates to align to.
-    fromXYZ : np.ndarray
+    from_xyz : np.ndarray
         A 3 x N array of coordinates to align.
 
     Returns
@@ -22,21 +22,17 @@ def kabsch(toXYZ: np.ndarray, fromXYZ: np.ndarray):
     ValueError
         If the arrays differ in the number of coordinates N.
     """
-    shape1, shape2 = fromXYZ.shape, toXYZ.shape
+    shape1, shape2 = from_xyz.shape, to_xyz.shape
 
     if shape1[1] != shape2[1]:
         raise ValueError("KABSCH: unequal array sizes")
 
-    m1 = np.mean(fromXYZ, 1).reshape((shape1[0], 1))
-    m2 = np.mean(toXYZ, 1).reshape((shape2[0], 1))
-    tmp1 = np.tile(m1, shape1[1])
-    tmp2 = np.tile(m1, shape2[1])
+    m1 = np.mean(from_xyz, 1).reshape((shape1[0], 1))
+    m2 = np.mean(to_xyz, 1).reshape((shape2[0], 1))
+    mean = np.tile(m1, shape1[1])
 
-    assert np.allclose(tmp1, tmp2)
-    assert tmp1.shape == fromXYZ.shape
-    assert tmp2.shape == toXYZ.shape
-    t1 = fromXYZ - tmp1
-    t2 = toXYZ - tmp2
+    t1 = from_xyz - mean
+    t2 = to_xyz - mean
 
     u, _, wh = np.linalg.svd(np.dot(t2, t1.T))
 
@@ -47,7 +43,7 @@ def kabsch(toXYZ: np.ndarray, fromXYZ: np.ndarray):
     T = m2 - np.dot(R, m1)
 
     tmp3 = np.reshape(np.tile(T, (shape2[1])), shape1)
-    err = toXYZ - np.dot(R, fromXYZ) - tmp3
+    err = to_xyz - np.dot(R, from_xyz) - tmp3
 
     eRMSD = np.sqrt(np.sum(err ** 2) / shape2[1])
     return R, T, eRMSD, err.T
